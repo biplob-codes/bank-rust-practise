6f9439cdf0bde58e4acea8dd996ce2ae65e045fb
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-07-25 04:34:46 +0600

    Defining structs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+#[derive(Debug)]
+struct Account {
+    id: u32,
+    holder: String,
+    balance: i32,
+}
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
 fn main() {
     println!("Hello, world!");
 }
