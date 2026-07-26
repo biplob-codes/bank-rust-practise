f4c74e718199b665a04b0ac4a861b021b46e5a92
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-07-27 05:42:02 +0600

    Using result enum

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
-use std::fs;
+use std::{fs, io::Error};
 fn main() {
-    let text = fs::read_to_string("logs.txt");
-    println!("{:#?}", text);
+    match divide(5.0, 0.0) {
+        Err(e) => {
+            println!("{}", e);
+        }
+        Ok(v) => {
+            println!("{}", v)
+        }
+    }
+}
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("Cannot divide by 0"))
+    } else {
+        Ok(a / b)
+    }
 }
