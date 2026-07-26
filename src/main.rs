4bf4959ba402338a7942b142a97b33e02c36824a
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-07-27 02:09:08 +0600

    Adding implementation to enums

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,34 @@ enum Media {
 fn print_media(m: Media) {
     println!("{:#?}", m)
 }
-
+impl Media {
+    fn description(&self) -> String {
+        if let Media::Book { title, author } = self {
+            format!("Book: {} - {}", title, author)
+        } else if let Media::Movie { title, director } = self {
+            format!("Movie: {} - {}", title, director)
+        } else if let Media::Audiobook { title } = self {
+            format! {"{}",title}
+        } else {
+            String::from("Media description")
+        }
+    }
+}
 fn main() {
     let ab = Media::Audiobook {
         title: String::from("Sharing is caring"),
     };
-    print_media(ab);
+    let b = Media::Book {
+        title: String::from("Operating Systems: Three Easy Pieces"),
+        author: String::from("Remzi and Andrea Arpaci-Dusseau"),
+    };
+
+    let m = Media::Movie {
+        title: String::from("The Oddessy"),
+        director: String::from("Christoger Nolan"),
+    };
+
+    println!("{}", ab.description());
+    println!("{}", b.description());
+    println!("{}", m.description());
 }
