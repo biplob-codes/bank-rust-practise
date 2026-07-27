8fb24cac74be7292a89e555bda73a19531e915ad
Md. Biplob Talukdar <biplob.codes@gmail.com>
2026-07-27 07:17:46 +0600

    Using result with reading file

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 use std::{fs, io::Error};
 fn main() {
-    match divide(5.0, 0.0) {
+    match fs::read_to_string("logs.txt") {
         Err(e) => {
-            println!("{}", e);
+            println!("Error:=> {}", e);
         }
         Ok(v) => {
             println!("{}", v)
         }
-    }
-}
-fn divide(a: f64, b: f64) -> Result<f64, Error> {
-    if b == 0.0 {
-        Err(Error::other("Cannot divide by 0"))
-    } else {
-        Ok(a / b)
     }
 }
